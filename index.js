3df1d1cb7a5968e945dcb15450be738400570f2e
OPaslavskiy <bankir.op@gmail.com>
2023-05-04 22:39:24 +0300

    add comand

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+const { program } = require("comander");
+
 const contacts = require("./contacts");
 
 const invokeAction = async ({ action, id, name, email, phone }) => {
@@ -16,3 +18,17 @@ const invokeAction = async ({ action, id, name, email, phone }) => {
       return console.log(removeContact);
   }
 };
+
+const program = new Command();
+program
+  .option("-a, --action <type>", "choose action")
+  .option("-i, --id <type>", "user id")
+  .option("-n, --name <type>", "user name")
+  .option("-e, --email <type>", "user email")
+  .option("-p, --phone <type>", "user phone");
+
+program.parse();
+
+const argv = program.opts();
+
+invokeAction(argv);
